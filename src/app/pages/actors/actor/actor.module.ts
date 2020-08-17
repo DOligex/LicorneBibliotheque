f51a0efc7f55f199a69ea100b26855c6b6bf86d3
DOligex <olinours@yahoo.fr>
2020-08-17 14:57:48 -0400

    build actor component

--- src/app/pages/actors/actor/actor.module.ts
+++ src/app/pages/actors/actor/actor.module.ts
@@ -3,6 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ActorRoutingModule } from './actor-routing.module';
 import { ActorComponent } from './actor.component';
+// material
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -10,6 +20,16 @@ import { ActorComponent } from './actor.component';
   imports: [
     CommonModule,
     ActorRoutingModule,
+    HttpClientModule,
+    // material
+    MatExpansionModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSortModule,
+    FormsModule,
+    MatProgressSpinnerModule,
   ]
 })
 export class ActorModule { }
