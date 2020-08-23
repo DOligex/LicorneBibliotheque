7bf733dfe6c52fd747bc0f22e39bb0cae9514ca1
DOligex <olinours@yahoo.fr>
2020-08-23 14:59:08 -0400

    fix: name public to public-

--- src/app/pages/public-/recap-kind-public/recap-kind-public.component.ts
+++ src/app/pages/public-/recap-kind-public/recap-kind-public.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-recap-kind-public',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecapKindPublicComponent implements OnInit {
 
-  constructor() { }
+  constructor(private title: Title) { }
 
   ngOnInit() {
+    this.title.setTitle('Liste des types de publics');
   }
 
 }
